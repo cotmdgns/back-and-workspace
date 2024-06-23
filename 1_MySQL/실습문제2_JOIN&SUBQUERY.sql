f57d26fe2863198a0754e8950434b95734926805
cotmdgns <qmflskzm@gmail.com>
2024-06-24 00:39:11 +0900

    06_24_복습

--- 1_MySQL/실습문제2_JOIN&SUBQUERY.sql
+++ 1_MySQL/실습문제2_JOIN&SUBQUERY.sql
@@ -68,4 +68,34 @@ HAVING email LIKE 'JOYCE%';
 
 
 
+
+SELECT * FROM category; -- 영화 카테고리 정보 : category_id
+SELECT * FROM rental; -- DVD 대여 정보 : customer_id, inventory_id
+SELECT * FROM inventory; -- DVD 대여점에서 관리하는 정보 : inventory_id, film_id
+SELECT * FROM film; -- 영화 정보 : film_id
+SELECT * FROM actor; -- 영화 배우 정보 : actor_id
+
+SELECT * FROM film_category; -- film과 category 연결 : film_id, category_id
+SELECT * FROM film_actor; -- film과 actor 연결 : film_id, actor_id
+
+-- DVD 대여 고객 정보 : customer_id, address_id, first_name, last_name
+SELECT * FROM customer; 
+
+SELECT * FROM address; -- 고객 주소 정보 : address_id, city_id, address, district
+SELECT * FROM city; -- city_id, country_id, city
+SELECT * FROM country; -- country_id, country
+
 -- 5. 이메일이 JOYCE.EDWARDS@sakilacustomer.org인 고객이 가장 최근에 빌린 영화 제목과 영화 내용을 조회 
+SELECT title,description ,rental_date
+FROM customer
+JOIN rental USING (customer_id)
+JOIN inventory USING (inventory_id)
+JOIN film_category USING (film_id)
+JOIN category USING (category_id)
+JOIN film USING (film_id)
+GROUP BY title,email,description,rental_date
+HAVING email LIKE 'JOYCE%'
+ORDER BY rental_date DESC
+LIMIT 1;
+
+
