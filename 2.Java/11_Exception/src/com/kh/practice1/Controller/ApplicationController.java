57ac64532125552703a438df0e45c8dbc687e456
cotmdgns <qmflskzm@gmail.com>
2024-07-16 01:05:04 +0900

    07_16

--- 2.Java/11_Exception/src/com/kh/practice1/Controller/ApplicationController.java
+++ 2.Java/11_Exception/src/com/kh/practice1/Controller/ApplicationController.java
@@ -11,6 +11,19 @@ public class ApplicationController {
 	Member m = new Member();
 	ArrayList<Member> list = new ArrayList();
 	
+	//부품
+	public int searchBox(String sing) {
+		for(int i = 0; i<list.size(); i++) {
+			if(list.get(i).getSing().equals(sing)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	// 연결 부품
+//		int box = searchBox(sing);
+//		if(box == -1) return "없습니다";
+	
 	public String insert(String sing, String singer) { // 마지막에 추가하기
 		for(int i = 0; i<list.size(); i++) {
 			if(list.get(i).getSing().equals(sing)) {
@@ -30,61 +43,26 @@ public class ApplicationController {
 		list.add(0,new Member(sing, singer));
 		return "추가 성공!";
 	}
-	
-	
-	
 	public String print() { // 곡 출력
 		String a = "";
 		for(int i = 0; i<list.size(); i++) {
-			a += "곡 명 :"+list.get(i).getSing()+"\n가수 명 :"+list.get(i).getSinger()+"\n";
-		}
+		a += "곡 명 :"+list.get(i).getSing()+"\n가수 명 :"+list.get(i).getSinger()+"\n";}
 		return a;
 	}
-	
-	
-//	/**/
-//	public int searchBox(String sing) {		//있는지 확인 박스
-//		for(int i = 0; i<list.size(); i++) {
-//			if(list.get(i).getSing().equals(sing)) {
-//				return i;
-//			} 
-//		}
-//		return -1;
-//	}
-//	
-//	public String searchOn(String sing) { //곡 검색하기
-//
-//			int a = searchBox(sing);
-//			if(a==-1) return "없어";
-//			return list.get(a).getSing()+"-"+list.get(a).getSinger()+"을 검색 했습니다.";
-//
-//	}
-	/**/
-	
-	
-	public String searchOn(String sing) { //곡 검색하기
-		for(int i = 0; i<list.size()+1; i++) {
-			if(i == list.size()) {
-				return "없습니다";
-				} 
-			if(list.get(i).getSing().equals(sing)) {
-				return list.get(i).getSing()+"-"+list.get(i).getSinger()+"을 검색 했습니다.";
-			}
-		}
-		return "없습니다";
+	public String searchOn(String sing) { //곡 검색하기	
+		int box = searchBox(sing);
+		if(box == -1) return "없습니다";
+		return list.get(box).getSing()+"-"+list.get(box).getSinger()+"을 검색 했습니다.";
 	}
 	public String remove(String deletesing) { //곡 지우기
-		for(int i = 0; i<list.size()+1; i++) {
-			if(i == list.size()) {
-				return "없습니다";
-				} 
-			if(list.get(i).getSing().equals(deletesing)) {
-				list.remove(i);
-				break;
-			}
+		int box = searchBox(deletesing);
+		if(box == -1) return "없습니다";
+		if(list.get(box).getSing().equals(deletesing)) {
+			list.remove(box);
 		}
 		return "삭제됬습니다";
 	}
+	
 	public String upperData(String searchsing,String sing,String singer) { //곡 업데이트하기
 		for(int i = 0; i<list.size()+1; i++) {
 			if(i == list.size()) {
--- 2.Java/cotmdgns/src/com/gdgd/Main.java
+++ 2.Java/cotmdgns/src/com/gdgd/Main.java
@@ -4,21 +4,10 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int H = sc.nextInt();
-		int M = sc.nextInt();
-		sc.nextLine();
-		int T = sc.nextInt();
-		int sum = 0;
-		H += (M+T) / 60;
-		sum = (M+T) % 60;
-		if((M+T)>=60) {
-			if(H > 23){
-				H -= 24;
-			}
-			System.out.println(H + " " + sum);
-		}else {
-			System.out.println(H + " " + sum);
+		int sum = 0,num = sc.nextInt();
+		for(int i = 1; i <= num; i++) {
+			sum+=i;
 		}
-		
+		System.out.println(sum);
 	}
 }
