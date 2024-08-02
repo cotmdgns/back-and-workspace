3c81cde07e30ef8262568b480b7c283e2773513b
cotmdgns <qmflskzm@gmail.com>
2024-08-02 17:51:33 +0900

    08_02

--- 1_MySQL/대한민국_위치_대분류_소분류.sql
+++ 1_MySQL/대한민국_위치_대분류_소분류.sql
@@ -22,8 +22,7 @@ drop table location_category_small;
 -- 위치대분류 데이터 삽입
 INSERT INTO location_category_large (loc_la_name) VALUES
 ('서울특별시'),('부산광역시'),('대구광역시'),('인천광역시'),('광주광역시'),('대전광역시'),('경기도'),
-('강원도'),('충청북도'),('충청남도'),('전라북도'),('전라남도'),('경상북도'),('경상남도'),('제주도'),('세종시');
-
+('강원도'),('충청북도'),('충청남도'),('전라북도'),('전라남도'),('경상북도'),('경상남도'),('제주도');
 -- 서울특별시 소분류
 INSERT INTO location_category_small (loc_s_name, loc_la_code) VALUES
 ('종로구', (SELECT loc_la_code FROM location_category_large WHERE loc_la_name = '서울특별시')),
--- 5_Spring/youtube-semi/src/main/java/com/semi/youtube/controller/pageController.java
+++ 5_Spring/youtube-semi/src/main/java/com/semi/youtube/controller/pageController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.semi.youtube.controller.service.VideoService;
 
@@ -17,5 +18,11 @@ public class pageController {
 	public String index(Model model){
 		model.addAttribute("list", video.allVideo());
 		return "index";
+	}
+	
+	@GetMapping("/{videoCode}")
+	public String detail(@PathVariable("videoCode") String videoCode) {
+		System.out.println(videoCode);
+		return "detail";
 	}
 }
